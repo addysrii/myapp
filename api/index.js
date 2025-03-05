6de6d0c192a99f0b2c52041dd6f6e620408c02c3
addysrii <149177246+addysrii@users.noreply.github.com>
2025-03-06 00:33:33 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,9 +31,9 @@ dotenv.config();
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
-const BASE_URL = process.env.BASE_URL || `https://myapp-uc9m.onrender.com`;
+const BASE_URL = `https://myapp-uc9m.onrender.com`;
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: 'https://appy-coral.vercel.app/', // Your frontend URL
   credentials: true
 }));
 // Environment variables
